7ab71f3173be7e879df04c734458b4a2fa119d3b
shreejay99 <shreejaychopde99@gmail.com>
2025-10-24 13:07:52 +0530

    add Util code

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,15 @@
+class ApiResponse extends Response{
+    constructor(
+        statusCode,
+        data,
+        message = "Success"
+ 
+    ){
+        this.statusCode = statusCode,
+        this.data = data,
+        this.message = message,
+        this.success = statusCode < 400
+    }
+}
+
+export {ApiResponse}
